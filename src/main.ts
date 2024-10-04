06bf199e0b61288159469bb70ba93c166d961ac1
Pavel Bystritsky <p.bystritskyi@gmail.com>
2024-10-04 14:35:13 +0200

    Base task done.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const numbers: number[] = [1 ,2 ,3, 4];
+console.log("Step 1: Create an array of numbers. Use the '.map' function to double all the numbers in the array.");
+console.log(numbers.map((num: number) => num * 2));
+const words: string[] = ['one', 'two', 'three'];
+console.log("Step 2: Create an array of words. Use the '.filter' function to select only the words that are longer than 3 letters.");
+console.log(words.filter((word: string) => word.length > 3));
+console.log("Step 3: Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.");
+console.log(numbers.reduce((acc: number, value: number) => acc + value));
+console.log("Step 4: Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.");
+console.log(numbers.some((value: number) => 10 < value));
